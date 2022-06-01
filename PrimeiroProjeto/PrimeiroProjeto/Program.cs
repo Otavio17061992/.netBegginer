534ef2a6e5295232eb9487f2d5c7a54c4c68ba02
Otavio  Mesquita <93503467+Otavio17061992@users.noreply.github.com>
2022-05-31 22:28:08 -0300

    Adicionar arquivos de projeto.

---
+++ PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace PrimeiroProjeto {
+    class Program {
+        static void Main(string[] args) {
+
+            double b, B, h, area;
+
+            b = 6.0;
+            B = 8.0;
+            h = 5.0;
+
+
+            area = (b + B) / 2.0 * h;
+
+
+            Console.WriteLine(area);
+
+
+        }
+    }
+}
