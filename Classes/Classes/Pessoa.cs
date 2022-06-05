2e98c978647b00f42f82b8b05a72bc482ce3c91c
Otavio  Mesquita <93503467+Otavio17061992@users.noreply.github.com>
2022-06-04 21:20:13 -0300

    Sistema de Deposito

---
+++ Classes/Classes/Pessoa.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+     class Pessoa
+    {
+        public string Nome { get; set; }
+        public string CPF { get; set; } 
+        public string NumeroContaCorrente { get; set; }
+
+
+        // Metodos (ações)
+
+        public double EfetuarDeposito(double valorDeposito)
+        {
+            double saldo = 100.0;
+            return  saldo = saldo + valorDeposito;
+        }
+    }
+}
---
+++ Classes/Classes/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa1 = new Pessoa();
+            pessoa1.Nome = "Ray Carneiro";
+            pessoa1.CPF = "1233223324";
+            pessoa1.NumeroContaCorrente = "0203040";
+
+            double valorDoDeposito;
+
+            Console.WriteLine(" Digite o Quanto deseja Depositar?");
+            valorDoDeposito = double.Parse(Console.ReadLine()); 
+
+            Console.WriteLine("O valor da conta corrente é de: "+ pessoa1.EfetuarDeposito(valorDoDeposito));
+           
+                
+
+
+        }
+    }
+}
